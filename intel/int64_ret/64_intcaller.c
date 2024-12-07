a23b9a1b87085d56dd28c75260f45e83efae57d9
fadhil riyanto <me@fadev.org>
2024-12-07 20:04:46 +0700

    int 64 overflow test

--- intel/int64_ret/64_intcaller.c
+++ intel/int64_ret/64_intcaller.c
@@ -11,7 +11,7 @@ int main()
 {
         __u64 int64_res = asm_func();
         printf("SGN: %" PRId64 "\n", (int64_t)int64_res);
-        printf("DEC: %" PRIu64 "\n", (int64_t)int64_res);
+        printf("DEC: %" PRIu64 "\n", (uint64_t)int64_res);
 
         do_exit();
 }
