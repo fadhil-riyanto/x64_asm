c0ebdb59e91e0abbc7a5008f3f820f906bdb04e3
fadhil riyanto <me@fadev.org>
2024-12-07 19:54:31 +0700

    test return value from func using rax register

---
+++ intel/int64_ret/64_intcaller.c
@@ -0,0 +1,17 @@
+#include <linux/types.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+__u64 asm_func();
+void print_binary(__u64 *ptr);
+void do_exit();
+
+int main()
+{
+        __u64 int64_res = asm_func();
+        printf("SGN: %" PRId64 "\n", (int64_t)int64_res);
+        printf("DEC: %" PRIu64 "\n", (int64_t)int64_res);
+
+        do_exit();
+}
